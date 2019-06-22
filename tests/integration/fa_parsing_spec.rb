0b06bf559a7f62f650b8804f11293d6a7bf8c609
joshcoales <joshua@coales.co.uk>
2019-06-22 21:17:29 +0100

    Implementing journals list tests

--- tests/integration/fa_parsing_spec.rb
+++ tests/integration/fa_parsing_spec.rb
@@ -10,6 +10,8 @@ describe 'FA parser' do
   TEST_USER_NOT_EXIST = "fafeed-does-not-exist"
   TEST_USER_OVER_200_WATCHERS = "fender"
   TEST_USER_NO_WATCHERS = "fafeed-no-watchers"
+  TEST_USER_NO_JOURNALS = TEST_USER_NO_WATCHERS
+  TEST_USER_OVER_25_JOURNALS = TEST_USER_OVER_200_WATCHERS
 
   before do
     config = File.exist?('settings-test.yml') ? YAML.load_file('settings-test.yml') : {}
@@ -276,12 +278,39 @@ describe 'FA parser' do
   end
 
   context 'when listing a user\'s journals' do
-    it 'returns a list of journal IDs'
-    it 'fails when given a non-existent user'
-    it 'handles an empty journal listing'
-    it 'displays a second page, different than the first'
-    it 'displays valid full data'
-    it 'pages full data correctly'
+    it 'returns a list of journal IDs' do
+      journals = @fa.journals(TEST_USER, 1)
+      expect(journals).to be_instance_of Array
+      expect(journals).not_to be_empty
+      journals.each do |journal|
+        expect(journal[:id]).to match(/^[0-9]+$/)
+        expect(journal[:title]).not_to be_blank
+        expect(journal[:description]).not_to be_blank
+        expect(journal[:link]).to eql("https://www.furaffinity.net/journal/#{journal[:id]}/")
+        check_date(journal[:posted], journal[:posted_at])
+      end
+    end
+
+    it 'fails when given a non-existent user' do
+      expect { @fa.journals(TEST_USER_NOT_EXIST, 1) }.to raise_error(FASystemError)
+    end
+
+    it 'handles an empty journal listing' do
+      journals = @fa.journals(TEST_USER_NO_JOURNALS, 1)
+      expect(journals).to be_instance_of Array
+      expect(journals).to be_empty
+    end
+
+    it 'displays a second page, different than the first' do
+      journals1 = @fa.journals(TEST_USER_OVER_25_JOURNALS, 1)
+      journals2 = @fa.journals(TEST_USER_OVER_25_JOURNALS, 2)
+      expect(journals1).to be_instance_of Array
+      expect(journals1).not_to be_empty
+      expect(journals1.length).to be 25
+      expect(journals2).to be_instance_of Array
+      expect(journals2).not_to be_empty
+      expect(journals1).not_to eql(journals2)
+    end
   end
 
   context 'when viewing user galleries' do
