a39653e331d2728591b27dbf6ef490b7c38ec38e
Deer Spangle <deer@spangle.org.uk>
2020-02-05 23:18:58 +0000

    Allowing a and b cookie to be specified in either order

--- lib/faexport.rb
+++ lib/faexport.rb
@@ -54,7 +54,7 @@ module FAExport
 
     USER_REGEX = /((?:[a-zA-Z0-9\-_~.]|%5B|%5D|%60)+)/
     ID_REGEX = /([0-9]+)/
-    COOKIE_REGEX = /^b=[a-z0-9\-]+; a=[a-z0-9\-]+$/
+    COOKIE_REGEX = /^([ab])=[a-z0-9\-]+; ?(?!\1)[ab]=[a-z0-9\-]+$/
     NOTE_FOLDER_REGEX = /(inbox|outbox|unread|archive|trash|high|medium|low)/
 
     def initialize(app, config = {})
