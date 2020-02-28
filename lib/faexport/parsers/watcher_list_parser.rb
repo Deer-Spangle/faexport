013a33f69858ff9a86cc41c4869833a0d837683a
Deer Spangle <deer@spangle.org.uk>
2020-02-28 19:53:53 +0000

    Pulling out watcher parsing to WatcherListParser

---
+++ lib/faexport/parsers/watcher_list_parser.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+
+# scraper.rb - Quick and dirty API for scraping data from FA
+#
+# Copyright (C) 2015 Erra Boothale <erra@boothale.net>
+# Further work: 2020 Deer Spangle <deer@spangle.org.uk>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of FAExport nor the names of its contributors may be
+#     used to endorse or promote products derived from this software without
+#     specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+require_relative 'parser'
+
+class WatcherListParser < Parser
+
+  def initialize(fetcher, user, page, is_watchers)
+    super(fetcher)
+    @user = user
+    @page = page
+    @is_watchers = is_watchers
+  end
+
+  def get_path
+    mode = @is_watchers ? 'to' : 'by'
+    "watchlist/#{mode}/#{escape(@user)}/#{@page}/"
+  end
+
+  def get_cache_key
+    mode = @is_watchers ? "watchers" : "watching"
+    "#{mode}:#{@user}:#{@page}"
+  end
+
+  def parse_classic(html)
+    error_msg = html.at_css("table.maintable td.alt1 b")
+    if !error_msg.nil? && error_msg.content == "Provided username not found in the database."
+      raise FASystemError.new(url)
+    end
+
+    html.css('.artist_name').map{|elem| elem.content}
+  end
+end
