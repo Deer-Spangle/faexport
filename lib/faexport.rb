6fb7ee26a8fa65b9def0dccabe0f36a49f274dfb
Deer-Spangle <Deer-Spangle@users.noreply.github.com>
2024-08-30 06:37:21 +0000

    Publishing version 2024.08.1

--- lib/faexport.rb
+++ lib/faexport.rb
@@ -46,7 +46,7 @@ require "prometheus/client"
 Tilt.register Tilt::RedcarpetTemplate, "markdown", "md"
 
 # Do not update this manually, the github workflow does it.
-VERSION = "2024.03.4"
+VERSION = "2024.08.1"
 prom = Prometheus::Client.registry
 version_info = prom.gauge(
   :faexport_info,
