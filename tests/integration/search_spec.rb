a7b87dae0d1b954feffc723e423da154d3a8589f
Deer-Spangle <deer@spangle.org.uk>
2025-12-12 00:44:03 +0000

    search_with_retry should also handle FASlowdownError, not just FAStatusError

--- tests/integration/search_spec.rb
+++ tests/integration/search_spec.rb
@@ -30,10 +30,10 @@ describe "FA parser search endpoint" do
 
     begin
       @fa.search(args)
-    rescue FAStatusError => e
+    rescue [FAStatusError, FASlowdownError] => e
       raise unless (retries += 1) <= 5
 
-      puts "FAStatusError on Search: #{e}, retry #{retries} in #{wait_between_tries} second(s)..."
+      puts "FA error on Search: #{e}, retry #{retries} in #{wait_between_tries} second(s)..."
       sleep(wait_between_tries)
       retry
     end
